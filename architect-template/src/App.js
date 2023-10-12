d74bc9194701408a55ccb35b8bb3b561ae65bc64
Akif Emre Senol <akifffemreee@gmail.com>
2023-10-13 00:28:13 +0300

    finish architect template project

--- architect-template/src/App.js
+++ architect-template/src/App.js
@@ -1,5 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import PageContent from "./components/PageContent";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <PageContent />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
