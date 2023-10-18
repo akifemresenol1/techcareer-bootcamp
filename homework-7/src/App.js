a2ff32f0a5d720d563ad4be3db02bf969a3e7ea1
Akif Emre Senol <akifffemreee@gmail.com>
2023-10-18 23:04:36 +0300

    finish homework 8

--- homework-7/src/App.js
+++ homework-7/src/App.js
@@ -1,9 +1,23 @@
 import Suppliers from "./homework-7/Suppliers";
+import Orders from "./homework-8/Orders";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Suppliers />
+      <ul style={{ display: "flex", justifyContent: "space-between" }}>
+        <li>
+          <Link to="/">Orders Homework 8</Link>
+        </li>
+        <li>
+          <Link to="/homework7">Suppliers Homework 7</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Orders />} />
+        <Route path="/homework7" element={<Suppliers />} />
+      </Routes>
     </>
   );
 }
